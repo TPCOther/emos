38cf7d508113799d441d959557fc92ea41c6fbd4
npcxh <bakanpcxh@gmail.com>
2021-07-08 17:24:14 +0800

    完成后端注册模块

--- src/main/java/com/example/emos/wx/db/dao/TbUserDao.java
+++ src/main/java/com/example/emos/wx/db/dao/TbUserDao.java
@@ -3,17 +3,13 @@ package com.example.emos.wx.db.dao;
 import com.example.emos.wx.db.pojo.TbUser;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.HashMap;
+import java.util.Set;
+
 @Mapper
 public interface TbUserDao {
-    int deleteByPrimaryKey(Integer id);
-
-    int insert(TbUser record);
-
-    int insertSelective(TbUser record);
-
-    TbUser selectByPrimaryKey(Integer id);
-
-    int updateByPrimaryKeySelective(TbUser record);
-
-    int updateByPrimaryKey(TbUser record);
+    public boolean haveRootUser();
+    public int insert(HashMap param);
+    public Integer searchIdByOpenId(String openId);
+    public Set<String> searchUserPermissions(int userId);
 }
