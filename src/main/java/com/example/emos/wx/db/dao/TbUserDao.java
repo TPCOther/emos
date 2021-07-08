1ab28641dff695bec4dd2086c8bc9ae32cb2b51a
npcxh <bakanpcxh@gmail.com>
2021-07-08 17:24:14 +0800

    完成后端注册模块

--- src/main/java/com/example/emos/wx/db/dao/TbUserDao.java
+++ src/main/java/com/example/emos/wx/db/dao/TbUserDao.java
@@ -3,17 +3,13 @@ package com.example.emos.wx.db.dao;
 import com.example.emos.wx.db.pojo.TbUser;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.HashMap;
+import java.util.Set;
+
 @Mapper
 public interface TbUserDao {
-    int deleteByPrimaryKey(Integer id);
-
-    int insert(TbUser record);
-
-    int insertSelective(TbUser record);
-
-    TbUser selectByPrimaryKey(Integer id);
-
-    int updateByPrimaryKeySelective(TbUser record);
-
-    int updateByPrimaryKey(TbUser record);
+    public boolean haveRootUser();  //是否已绑定超级管理员
+    public int insert(HashMap param); //插入用户数据
+    public Integer searchIdByOpenId(String openId); //查询用户ID
+    public Set<String> searchUserPermissions(int userId); //查询用户权限，set用于多角色权限去重
 }
---
+++ src/main/java/com/example/emos/wx/service/UserService.java
@@ -0,0 +1,9 @@
+package com.example.emos.wx.service;
+
+import java.util.Set;
+
+public interface UserService {
+    public int registerUser(String registerCode, String code, String nickname, String photo);   //用户注册
+
+    public Set<String> searchUserPermissions(int userId); //权限查询
+}
